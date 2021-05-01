16365f285082bd91912571a2edad2bc31bf6946e
limelier <tudor.iacobescu.1@gmail.com>
2021-05-01 22:32:50 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Minecraft image creator
+
+Simple image printing script that connects to a server via rcon and 
+uses `/setblock` to print an image up to 256x256 in size.
+
+Thanks to [RedHotRojo](https://github.com/RedHotRojo/Python-Draws-in-Minecraft)
+for the inspiration, and for the list of blocks.
+
+Possible improvements:
+- a better list of blocks, with only solid blocks and taking into account 
+blocks that have different textures on different sides
---
+++ main.py
@@ -0,0 +1,86 @@
+from math import sqrt, floor
+from typing import Dict, Set
+from mcipc.rcon.je import Client
+from PIL import Image
+
+x = 0
+y = 4
+z = 0
+
+
+class Color:
+    def __init__(self, r, g, b):
+        self.r = r
+        self.g = g
+        self.b = b
+
+    def distance(self, other):
+        return sqrt(
+            (self.r - other.r) ** 2 + \
+            (self.g - other.g) ** 2 + \
+            (self.b - other.b) ** 2
+        )
+
+
+class Block:
+    def __init__(self, line):
+        parts = line.strip().split(' ')
+        self.name = parts[0]
+        r, g, b = (int(x) for x in parts[1:])
+        self.color = Color(r, g, b)
+
+
+def read_blocks():
+    """Read the blocks from blocks.txt, returning them as a set Block instances."""
+    blocks = set()
+    with open('blocks.txt') as file:
+        for line in file:
+            blocks.add(Block(line))
+    return blocks
+
+
+def nearest_block(nearest_so_far: Dict[Color, Block], blocks: Set[Block], color):
+    """
+    Return the block corresponding to the color in `nearest_so_far` if one exists, or find the closest block in
+    `blocks`, add it to the dict, and then return it if it was not already there.
+    """
+    if color in nearest_so_far:
+        return nearest_so_far.get(color)
+    else:
+        block = min(
+            blocks,
+            key=lambda b: color.distance(b.color)
+        )
+        nearest_so_far[color] = block
+        return block
+
+
+def main():
+    blocks = read_blocks()
+
+    with Image.open('image.jpg') as img:
+        width, height = img.size
+        if width > 256 or height > 256:
+            print(f'Image too big ({width}x{height}), scaling down.')
+            biggest = max(width, height)
+            factor = 256 / biggest
+            width = floor(width * factor)
+            height = floor(height * factor)
+            img = img.resize((width, height))
+            print(f'Resized image to {width}x{height}.')
+        pixels = img.load()
+
+    nearest_so_far = {}  # cache found blocks to prevent future lookups - works best with flat-color images
+    with Client('127.0.0.1', 25575, passwd='cuzco') as client:
+        print(f'Starting to print {width} columns')
+        for i in range(width):
+            print(f'Printing col {i}...')
+            for j in range(height):
+                r, g, b = pixels[i, j]
+                color = Color(r, g, b)
+                block = nearest_block(nearest_so_far, blocks, color).name
+                client.setblock((x+i, y, z+j), f'minecraft:{block}')
+
+
+if __name__ == '__main__':
+    main()
